3c24ef477472424833a432a6e519d9f7ad1f8bf1
hades <hades@hadess-MacBook-Air.local>
2024-02-05 23:12:40 +0800

    Details

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+   <li class="minus">
+     Cash <span>-$400.00</span><button class="delete-btn">x</button>
+   </li>
+
+    <li class="plus">
+      Paycheck <span>$800</span><button class="add-btn">+</button>
+    </li>
+
+
+  </ul>
+</template>
+<script setup>
+</script>
